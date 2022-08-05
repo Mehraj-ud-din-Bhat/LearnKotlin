a9697144621d851f2ae6365d610af38d349d57dc
mehraj <mehraj@cdse-it.com>
2022-08-05 14:24:20 +0530

    -task app design

--- app/src/main/java/com/example/learnkotlin/MainActivity.kt
+++ app/src/main/java/com/example/learnkotlin/MainActivity.kt
@@ -8,30 +8,25 @@ import android.view.View
 import android.widget.*
 
 class MainActivity : AppCompatActivity() {
-     lateinit var  layout1:RelativeLayout
-     lateinit var  layout2:RelativeLayout
-     lateinit var  submit:Button
-     lateinit var  logout:Button
+      lateinit var  toolbarTitle:TextView;
+    lateinit var toolbarIcon: ImageView
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        layout1=findViewById(R.id.layout_1)
-        layout2=findViewById(R.id.layout_2)
-        submit=findViewById(R.id.signinbutton)
-        logout=findViewById(R.id.logout)
+        toolbarTitle=findViewById(R.id.toolBarTitle)
+        toolbarIcon=findViewById(R.id.toolbarIcon)
+        toolbarTitle.text="Task Manager"
 
-        layout1.visibility= View.VISIBLE
-        layout2.visibility= View.GONE
-        submit.setOnClickListener {
-            layout1.visibility= View.GONE
-            layout2.visibility= View.VISIBLE
+        toolbarIcon.setOnClickListener {
+            var intent=Intent(this,SecondActivity::class.java);
+            startActivity(intent)
+
         }
 
-        logout.setOnClickListener {
-            layout1.visibility= View.VISIBLE
-            layout2.visibility= View.GONE
-        }
+
 
     }
 
--- app/src/main/java/com/example/learnkotlin/SecondActivity.kt
+++ app/src/main/java/com/example/learnkotlin/SecondActivity.kt
@@ -2,17 +2,25 @@ package com.example.learnkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
-    lateinit var info:TextView
+    lateinit var  toolbarTitle:TextView;
+    lateinit var toolbarIcon: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-        info=findViewById(R.id.info)
-        var intentData=intent;
-        var name=intentData.getStringExtra("name");
-        info.text="I AM SECOND ACTIVITY: MAIN ACTIVITY SENT ME THIS $name"
+        toolbarTitle=findViewById(R.id.toolBarTitle)
+        toolbarIcon=findViewById(R.id.toolbarIcon)
+        toolbarIcon.setImageDrawable(getDrawable(R.drawable.icon_close))
+
+        toolbarIcon.setOnClickListener {
+            finish()
+        }
+
+        toolbarTitle.text="Add New Task"
+
 
 
     }
