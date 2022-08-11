9e823498384d5678bf4aeb494c73d1a776805d4e
mehraj <mehraj@cdse-it.com>
2022-08-11 18:43:52 +0530

    -ui part

--- app/src/main/java/com/example/learnkotlin/MainActivity.kt
+++ app/src/main/java/com/example/learnkotlin/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.learnkotlin
 
 import android.content.Intent
+import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,9 +9,9 @@ import android.view.View
 import android.widget.*
 
 class MainActivity : AppCompatActivity() {
-      lateinit var  toolbarTitle:TextView;
-      lateinit var toolbarIcon: ImageView
-      lateinit var tasks_tv:TextView
+    lateinit var b1:Button
+    lateinit var b2:Button
+
       companion object{
           var list:ArrayList<Task> = ArrayList();
 
@@ -24,18 +25,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        toolbarTitle=findViewById(R.id.toolBarTitle)
-        toolbarIcon=findViewById(R.id.toolbarIcon)
-        tasks_tv=findViewById(R.id.tasks_tv)
+        b1=findViewById(R.id.b1)
+        b2=findViewById(R.id.b2)
+        b1.setOnClickListener {
+            b1.backgroundTintList= ColorStateList.valueOf(resources.getColor(R.color.pink));
+            b1.setTextColor(resources.getColor(R.color.white))
 
-
-        toolbarTitle.text="Task Manager"
-
-        toolbarIcon.setOnClickListener {
-            var intent=Intent(this,SecondActivity::class.java);
-            startActivity(intent)
-
+            b2.backgroundTintList= ColorStateList.valueOf(resources.getColor(R.color.white));
+            b2.setTextColor(resources.getColor(R.color.black))
         }
+        b2.setOnClickListener {
+            b2.backgroundTintList= ColorStateList.valueOf(resources.getColor(R.color.pink));
+            b2.setTextColor(resources.getColor(R.color.white))
+
+            b1.backgroundTintList= ColorStateList.valueOf(resources.getColor(R.color.white));
+            b1.setTextColor(resources.getColor(R.color.black))
+        }
+
+
+
 
 
 
@@ -48,11 +56,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        tasks_tv.text="";
-        for(task in list)
-        {
-            tasks_tv.text=tasks_tv.text.toString() + task.taskDescription+"\n";
-        }
+
+
+
+
     }
 
 
