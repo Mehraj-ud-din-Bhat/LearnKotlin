a8208bffcc2ce91244c24b3be1a1eb617205d809
mehraj <mehraj@cdse-it.com>
2022-09-02 14:46:58 +0530

    Fragments

--- app/src/main/java/com/example/learnkotlin/MainActivity.kt
+++ app/src/main/java/com/example/learnkotlin/MainActivity.kt
@@ -8,13 +8,15 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
-     lateinit var userName:EditText
-     lateinit var userEmail:EditText
-     lateinit var userPhone:EditText
-     lateinit var userAddress:EditText
-     lateinit var submit:Button
+    lateinit var  framentContainer:FragmentContainerView
+    lateinit var  login:Button
+    lateinit var  signup:Button
 
 
 
@@ -22,107 +24,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        userName=findViewById(R.id.userName)
-        userEmail=findViewById(R.id.userEmail);
-        userPhone=findViewById(R.id.userMobile)
-        userAddress=findViewById(R.id.userAddress)
-        submit=findViewById(R.id.submit)
-        submit.setOnClickListener {
-            var status=validate();
-
-            if(status==true)
-            {
-                // Everything is fine proceed further
-                Toast.makeText(this,"I am ready to send the form",Toast.LENGTH_LONG).show()
-
-               moveScreen()
+        login=findViewById(R.id.login)
+        signup=findViewById(R.id.signup)
+        framentContainer=findViewById(R.id.fragmentContainer)
 
 
 
-            }
-
+        login.setOnClickListener {
+            replaceFragment(FragmentLogin())
         }
 
-        Log.d("ACTIVITY STATUS: ","ON CREATE");
-
-
-
-
-
-
-
-    }
-
-
-    override fun onStart() {
-        super.onStart()
-
-        Log.d("ACTIVITY STATUS: ","ON START CALLED");
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d("ACTIVITY STATUS: ","ON RESUME ");
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d("ACTIVITY STATUS: ","ON PAUSE");
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d("ACTIVITY STATUS: ","ON STOP");
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("ACTIVITY STATUS: ","ON DESTROY");
-    }
-
-
-    fun  moveScreen()
-    {
-        var intent=Intent(this,ViewInfo::class.java)
-        intent.putExtra("name",userName.text.toString())
-        intent.putExtra("email",userEmail.text.toString())
-        intent.putExtra("phone",userPhone.text.toString())
-        intent.putExtra("address",userAddress.text.toString())
-        startActivity(intent)
-    }
-
-
-    fun  validate():Boolean{
-
-        if(userName.text.toString().isEmpty())
-        {
-           userName.setError("Please enter user name")
-            return  false
+        signup.setOnClickListener {
+            replaceFragment(SignupFragment())
         }
 
 
-        if(userEmail.text.toString().isEmpty())
-        {
-            userEmail.setError("Please enter Email")
-            return  false
-        }
-
-
-        if(userPhone.text.toString().isEmpty())
-        {
-            userPhone.setError("Please enter your phone number")
-            return  false
-        }
-
-
-        if(userAddress.text.toString().isEmpty())
-        {
-            userAddress.setError("Please enter address")
-            return  false
-        }
-
-        return  true;
-
-
     }
+
+    fun replaceFragment(fragment: Fragment) {
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment, "TAG")
+        //    fragmentTransaction.addToBackStack(fragment.toString())
+        fragmentTransaction.commit()
+    }
+
+
+
 }
